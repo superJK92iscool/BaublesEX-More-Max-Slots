37ffa362748738bef9c66bd14a08c3cda6efe1dc
superJK92iscool <50285310+superJK92iscool@users.noreply.github.com>
2024-10-05 19:36:28 -0500

    Update Baubles.java

--- src/main/java/baubles/common/Baubles.java
+++ src/main/java/baubles/common/Baubles.java
@@ -30,7 +30,7 @@ public class Baubles {
 
     public static final String MODID = "baubles";
     public static final String MODNAME = "Baubles";
-    public static final String VERSION = "2.0.0";
+    public static final String VERSION = "2.0.1";
 
     @SidedProxy(clientSide = "baubles.client.ClientProxy", serverSide = "baubles.common.CommonProxy")
     public static CommonProxy proxy;
